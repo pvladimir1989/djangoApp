e4f31f7a3aeb3a3739365bc2c6251f48b97a9cd0
Vladimir <vovchikp.1989@gmail.com>
2021-08-22 12:14:15 +0300

    oauth done

--- testapp/urls/__init__.py
+++ testapp/urls/__init__.py
@@ -1,4 +1,6 @@
 from .urls import urlpatterns as base_urlpatterns
+from .oauth import urlpatterns as oauth_urlpatterns
 
 urlpatterns = []
 urlpatterns += base_urlpatterns
+urlpatterns += oauth_urlpatterns
